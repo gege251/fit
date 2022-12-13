961701cfd6c2231a1ab35ab5fb53411f9af8bf91
Szabo Gergely <gege251@mailbox.org>
2022-12-13 19:28:08 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,43 @@
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: CC0-1.0
+
 {
+  description = "fit";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        packageName = "fit";
       in
       {
-        devShell = pkgs.mkShell
-          {
-            buildInputs = [
-              pkgs.hlint
-              pkgs.haskellPackages.fourmolu
-            ];
+        packages.${packageName} =
+          haskellPackages.callCabal2nix packageName self rec {
+            # Dependency overrides go here
           };
-      }
 
-    );
+        defaultPackage = self.packages.${system}.${packageName};
 
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            haskellPackages.haskell-language-server # you must build it with your ghc to work
+            cabal-install
+            haskellPackages.fourmolu
+            hlint
+          ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
 }
