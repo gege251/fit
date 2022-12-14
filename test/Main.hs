8799a392919107d11c61fd0cbbcffff893f9c744
Szabo Gergely <gege251@mailbox.org>
2022-12-14 10:15:24 +0100

    Reorganising test files

--- test/Main.hs
+++ test/Main.hs
@@ -1,3 +1,5 @@
+module Main where
+
 import qualified Test.Fit.Internal.FitFile as FitFile
 import qualified Test.Fit.Internal.FitParser as FitParser
 import qualified Test.Fit.Internal.Numbers as Numbers
