9b84204d46d8a2a990d523635a0a9a90f1577c26
Szabo Gergely <gege251@mailbox.org>
2023-02-27 00:03:26 +0100

    Add default.nix for compatibility

--- flake.nix
+++ flake.nix
@@ -8,9 +8,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         compiler = "ghc924";
@@ -18,21 +22,19 @@
 
         haskellPackages = pkgs.haskell.packages.${compiler};
 
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
 
         packageName = "fit";
       in
       {
         packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
+          haskellPackages.callCabal2nix packageName self {
             # Dependency overrides go here
           };
 
         defaultPackage = self.packages.${system}.${packageName};
 
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
+          buildInputs = [
             haskellPackages.ghc
             haskellPackages.haskell-language-server # you must build it with your ghc to work
             haskellPackages.cabal-install
